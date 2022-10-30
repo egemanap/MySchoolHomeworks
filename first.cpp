22ebf4e3c24f06358a78f8e7a230fcfdd1809f0e
egemanap <117024288+egemanap@users.noreply.github.com>
2022-10-30 17:16:15 +0300

    Add files via upload
    My First Homework andn My First commit

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    string ad = "AYSE";
+    int haftalik = 100;
+    int yas = 14;
+    int hesap = (52 * 100) * (yas -10);
+    std::cout << "Benim Adim" <<ad<<"10 yasimdan beri ailemden haftalik"<<haftalik<<"TL aldim. Simdi"<<yas<<"yasindayim. Toplam"<<hesap<<"Tl aldim"<<std::endl;
+}
+
+
+
+
